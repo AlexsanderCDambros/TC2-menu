22f3bbbf71e224fbea0e991a441f19bdcbdf1320
Alexsander Chagas Dambros <alexsanderdambros@gmail.com>
2025-08-01 11:34:02 -0300

    chore: rollback CORS inuteis

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
-const path = require("path");
-const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = (webpackConfigEnv, argv) => {
   const defaultConfig = singleSpaDefaults({
@@ -14,27 +12,5 @@ module.exports = (webpackConfigEnv, argv) => {
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
-    output: {
-      path: path.resolve(__dirname, "dist"), // Garante a saída na pasta dist
-      publicPath: "/",
-    },
-    plugins: [
-      new CopyPlugin({
-        patterns: [
-          { 
-            from: path.resolve(__dirname, "public/_headers"),
-            to: path.resolve(__dirname, "dist"),
-            force: true  // Sobrescreve se existir
-          }
-        ]
-      })
-    ],
-    devServer: {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-        "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
-      }
-    }
   });
 };
